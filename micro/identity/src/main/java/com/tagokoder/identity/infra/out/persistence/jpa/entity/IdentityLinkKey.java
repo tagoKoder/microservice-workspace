34509d6e14777b5f5d1abb9a90e5c52c76cd7049
Santiago Tumbaco <santiago.tumbaco@integritysolutions.com.ec>
2026-02-03 16:55:54 -0500

    fixing identity 2

--- micro/identity/src/main/java/com/tagokoder/identity/infra/out/persistence/jpa/entity/IdentityLinkKey.java
+++ micro/identity/src/main/java/com/tagokoder/identity/infra/out/persistence/jpa/entity/IdentityLinkKey.java
@@ -1,10 +1,13 @@
 package com.tagokoder.identity.infra.out.persistence.jpa.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 import java.util.UUID;
 
 @Embeddable
+@EqualsAndHashCode
 public class IdentityLinkKey implements Serializable {
     @Column(name="identity_id")
     private UUID identityId;
